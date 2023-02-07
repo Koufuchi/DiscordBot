c6d0b10869890db01fe4eb00f120bb3d9db4bc37
Koufuchi <jackkao8752@gmail.com>
2023-02-08 01:18:42 +0800

    add openai gpt

--- main.py
+++ main.py
@@ -3,14 +3,15 @@
 # Import discord.py. Allows access to Discord's API.
 import discord
 
-# Import the os module.
 import os
 
-# Import load_dotenv function from dotenv module.
 from dotenv import load_dotenv
 
-# Import commands from the discord.ext module.
 from discord.ext import commands
+
+import openai
+
+import re
 
 
 # Loads the .env file that resides on the same level as the script.
@@ -19,37 +20,53 @@ load_dotenv()
 # Grab the API token from the .env file.
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 # Creates a new Bot object with a specified prefix. It can be whatever you want it to be.
 # all() 表示所有權限
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # on_message() event listener. Notice it is using @bot.event as opposed to @bot.command().
-# @bot.event
-# async def on_message(message):
-# 	# Check if the message sent to the channel is "hello".
-# 	if message.content == "hello":
-# 		# Sends a message to the channel.
-# 		await message.channel.send("pies are better than cakes. change my mind.")
+@bot.event
+async def on_message(message):
 
-# 	# Includes the commands for the bot. Without this line, you cannot trigger your commands.
-# 	await bot.process_commands(message)
+	#match = re.search('(?<=!小孤獨\s).+', message.content) #prompt=match.group(0),
+	prefix = message.content[0:5]
+
+	if prefix == '!小孤獨 ':
+		print(message.content)
+		print(message.content[5:])
+
+		response = openai.Completion.create(
+			model="text-davinci-003",
+			prompt=message.content[5:],
+			temperature=0, # 隨機文字組合，範圍 0～1，預設 0.5，0 表示不隨機，1 表示完全隨機。
+			max_tokens=3900, # 希望 AI 回傳的最大字數
+		)
+
+		# Sends a message to the channel.
+		await message.channel.send(response.choices[0].text)
+
+	# Includes the commands for the bot. Without this line, you cannot trigger your commands.
+	#await bot.process_commands(message)
 
 
 # Command $ping. Invokes only when the message "$ping" is send in the Discord server.
 # Alternatively @bot.command(name="ping") can be used if another function name is desired.
-@bot.command(
-	# Adds this value to the $help ping message.
-	help="Uses come crazy logic to determine if pong is actually the correct value or not.",
-	# Adds this value to the $help message.
-	brief="Prints pong back to the channel."
-)
+@bot.command()
 async def testPing(ctx):
 	# Sends a message to the channel using the Context object.
 	await ctx.channel.send("pong")
 
 
-@bot.command()
+@bot.command(
+	# Adds this value to the $help ping message.
+	help="輸入 !pin 和 dc 訊息 ID 來釘選",
+	# Adds this value to the $help message.
+	brief="輸入 !pin 和 dc 訊息 ID 來釘選"
+)
 async def pin(ctx, message_id: int):
     message = await ctx.fetch_message(message_id)
     await message.pin()
